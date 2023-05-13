afec46fd94c0a51621f47ca43d97e8cfb50ed82e
Mark Gray <mfgray87@gmail.com>
2023-05-13 09:09:39 +0100

    changes for getting deployed to azure

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from config import Config
 from flask_cors import CORS
 
-
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
